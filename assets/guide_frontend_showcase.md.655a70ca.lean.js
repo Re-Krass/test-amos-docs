3fbea7e1d2afd76a683342ab28c94cf897d3b0bd
Re-Krass <38668040+Re-Krass@users.noreply.github.com>
2022-02-16 22:08:03 +0100

    format files and edit links

--- assets/guide_frontend_showcase.md.655a70ca.lean.js
+++ assets/guide_frontend_showcase.md.655a70ca.lean.js
@@ -1,1 +1,61 @@
-import{_ as i,o as n,c as o,t as c,b as a,e as p,a as d,d as t,p as m,f as h}from"./app.9b460b2f.js";var v="/mendix/Login.png",g="/mendix/Registration.png",u="/mendix/Similarity-search-01.png",b="/mendix/Similarity-search-02.png",y="/mendix/Similarity-search-03.png",_="/mendix/Filters-01.png",f="/mendix/Filters-02.jpg",w="/mendix/Filters-03.png",x="/mendix/Filters-04.png",S="/mendix/Detailed-view-component.png",A="/mendix/Add-similarity-suggestion.png",T="/mendix/Select-component-type.png",k="/mendix/Add-component-01.png",C="/mendix/Add-component-02.png",I="/mendix/Added-components.png";const D={class:"button-key"},O={props:{k:String},setup(e){return(s,l)=>(n(),o("span",D,c(e.k),1))}};var N=i(O,[["__scopeId","data-v-ecb5efa4"]]);const r=e=>(m("data-v-0ce25052"),e=e(),h(),e),F=d("",33),R=t("This field has an auto complete. If you type for example "),V=r(()=>a("code",null,"he",-1)),P=t(" it complete it to "),L=r(()=>a("code",null,"height",-1)),B=t(" if you use the "),U=t(" key."),j=d("",26),z='{"title":"Showcase","description":"","frontmatter":{"sidebarDepth":3},"headers":[{"level":2,"title":"Demo Video","slug":"demo-video"},{"level":2,"title":"Login","slug":"login"},{"level":2,"title":"Registration","slug":"registration"},{"level":2,"title":"Similarity Search","slug":"similarity-search"},{"level":3,"title":"Apply Filters","slug":"apply-filters"},{"level":3,"title":"Show All Attributes","slug":"show-all-attributes"},{"level":3,"title":"Add Similarity Suggestion","slug":"add-similarity-suggestion"},{"level":3,"title":"Add New Component (Select Component Type)","slug":"add-new-component-select-component-type"},{"level":3,"title":"Add New Component","slug":"add-new-component"},{"level":2,"title":"Added Component Page","slug":"added-component-page"}],"relativePath":"guide/frontend/showcase.md","lastUpdated":1644447714413}',E={},q=Object.assign(E,{setup(e){return(s,l)=>(n(),o("div",null,[F,a("p",null,[R,V,P,L,B,p(N,{k:"Tab"}),U]),j]))}});var H=i(q,[["__scopeId","data-v-0ce25052"]]);export{z as __pageData,H as default};
+import {
+  _ as i,
+  o as n,
+  c as o,
+  t as c,
+  b as a,
+  e as p,
+  a as d,
+  d as t,
+  p as m,
+  f as h,
+} from "./app.9b460b2f.js";
+var v = "/mendix/Login.png",
+  g = "/mendix/Registration.png",
+  u = "/mendix/Similarity-search-01.png",
+  b = "/mendix/Similarity-search-02.png",
+  y = "/mendix/Similarity-search-03.png",
+  _ = "/mendix/Filters-01.png",
+  f = "/mendix/Filters-02.jpg",
+  w = "/mendix/Filters-03.png",
+  x = "/mendix/Filters-04.png",
+  S = "/mendix/Detailed-view-component.png",
+  A = "/mendix/Add-similarity-suggestion.png",
+  T = "/mendix/Select-component-type.png",
+  k = "/mendix/Add-component-01.png",
+  C = "/mendix/Add-component-02.png",
+  I = "/mendix/Added-components.png";
+const D = { class: "button-key" },
+  O = {
+    props: { k: String },
+    setup(e) {
+      return (s, l) => (n(), o("span", D, c(e.k), 1));
+    },
+  };
+var N = i(O, [["__scopeId", "data-v-ecb5efa4"]]);
+const r = (e) => (m("data-v-0ce25052"), (e = e()), h(), e),
+  F = d("", 33),
+  R = t(""),
+  V = r(() => a("code", null, "he", -1)),
+  P = t(""),
+  L = r(() => a("code", null, "height", -1)),
+  B = t(""),
+  U = t(""),
+  j = d(""),
+  z =
+    '{"title":"Showcase","description":"","frontmatter":{"sidebarDepth":3},"headers":[{"level":2,"title":"Demo Video","slug":"demo-video"},{"level":2,"title":"Login","slug":"login"},{"level":2,"title":"Registration","slug":"registration"},{"level":2,"title":"Similarity Search","slug":"similarity-search"},{"level":3,"title":"Apply Filters","slug":"apply-filters"},{"level":3,"title":"Show All Attributes","slug":"show-all-attributes"},{"level":3,"title":"Add Similarity Suggestion","slug":"add-similarity-suggestion"},{"level":3,"title":"Add New Component (Select Component Type)","slug":"add-new-component-select-component-type"},{"level":3,"title":"Add New Component","slug":"add-new-component"},{"level":2,"title":"Added Component Page","slug":"added-component-page"}],"relativePath":"guide/frontend/showcase.md","lastUpdated":1644447714413}',
+  E = {},
+  q = Object.assign(E, {
+    setup(e) {
+      return (s, l) => (
+        n(),
+        o("div", null, [
+          F,
+          a("p", null, [R, V, P, L, B, p(N, { k: "Tab" }), U]),
+          j,
+        ])
+      );
+    },
+  });
+var H = i(q, [["__scopeId", "data-v-0ce25052"]]);
+export { z as __pageData, H as default };
